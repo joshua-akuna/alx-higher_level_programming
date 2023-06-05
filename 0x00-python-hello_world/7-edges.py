12de6cfccc3082a0f091e4d0ee1abc470e3ad370
Joshua Akuna <akunajoshua@gmail.com>
2023-06-05 20:49:56 +0100

    0x00-python-hello_world : 7-edges.py

---
+++ 0x00-python-hello_world/7-edges.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+word = "Holberton"
+str = word[:3]
+print(f"First 3 letters: {str}")
+str = word[-2:]
+print(f"Last 2 letters: {str}")
+str = word[1:-1]
+print(f"Middle word: {str}")
