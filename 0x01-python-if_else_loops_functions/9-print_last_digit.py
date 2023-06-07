86ee43c3b50db06cb3cff60d6b6dfce8ed1cd132
Joshua Akuna <akunajoshua@gmail.com>
2023-06-07 03:39:06 +0100

    9-print_last_digit.py file commit : correction 3

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def print_last_digit(number):
     if (number < 0):
-        print((10 - (number % 10)) % 10 * -1)
+        print((10 - (number % 10)) % 10)
     else:
         print(number % 10)
