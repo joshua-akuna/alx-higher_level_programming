55b8be5540a353f77332391571fc0da7d01b796e
Joshua Akuna <akunajoshua@gmail.com>
2023-06-07 04:53:01 +0100

    100-print_tebahpla.py file commit

---
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+for letter in range(ord('z'), ord('a') - 1, -1):
+    offset = 0
+    if letter % 2 == 1:
+        offset = 32
+    print('{}'.format(chr(letter - offset)), end='')
+print()
