35411a7e5c3b60df3b5ba4b66d8c33584fc19254
Joshua Akuna <akunajoshua@gmail.com>
2023-06-07 04:05:37 +0100

    11-pow.py file commit

--- 0x01-python-if_else_loops_functions/11-pow.py
+++ 0x01-python-if_else_loops_functions/11-pow.py
@@ -1,6 +1,12 @@
 #!/usr/bin/python3
 def pow(a, b):
     res = 1
-    for i in range(b):
-        res = res * a
+    if b < 0:
+        for i in range(b, 0):
+            res = res * 1/a
+    elif b > 0:
+        for i in range(0, b):
+            res = res * a
+    else:
+        res = a
     return (res)
