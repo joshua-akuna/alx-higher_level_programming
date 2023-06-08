05f0e36851b44589861b1bbde88dc47b60bb07c8
Joshua Akuna <akunajoshua@gmail.com>
2023-06-08 23:02:57 +0100

    1-calculation.py file commit 2

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -6,5 +6,5 @@ b = 5
 
 print("{:d} + {:d} = {:d}".format(a, b, res.add(a, b)))
 print("{:d} - {:d} = {:d}".format(a, b, res.sub(a, b)))
-print("{:d} - {:d} = {:d}".format(a, b, res.mul(a, b)))
-print("{:d} - {:d} = {:d}".format(a, b, int(res.div(a, b))))
+print("{:d} * {:d} = {:d}".format(a, b, res.mul(a, b)))
+print("{:d} / {:d} = {:d}".format(a, b, int(res.div(a, b))))
