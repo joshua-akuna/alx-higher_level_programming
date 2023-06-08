2dd48c25c397d7b40a65ef859a738c77dea4d5b4
Joshua Akuna <akunajoshua@gmail.com>
2023-06-08 23:18:18 +0100

    1-calculation.py file commit 3

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
-res = __import__('calculator_1')
+if __name__ == "__main__":
+    res = __import__('calculator_1')
 
-a = 10
-b = 5
+    a = 10
+    b = 5
 
-print("{:d} + {:d} = {:d}".format(a, b, res.add(a, b)))
-print("{:d} - {:d} = {:d}".format(a, b, res.sub(a, b)))
-print("{:d} * {:d} = {:d}".format(a, b, res.mul(a, b)))
-print("{:d} / {:d} = {:d}".format(a, b, int(res.div(a, b))))
+    print("{:d} + {:d} = {:d}".format(a, b, res.add(a, b)))
+    print("{:d} - {:d} = {:d}".format(a, b, res.sub(a, b)))
+    print("{:d} * {:d} = {:d}".format(a, b, res.mul(a, b)))
+    print("{:d} / {:d} = {:d}".format(a, b, int(res.div(a, b))))
