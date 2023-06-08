85b34e64aabfbdcbb0c72dc30ab9fbdf03f06b23
Joshua Akuna <akunajoshua@gmail.com>
2023-06-08 23:11:59 +0100

    0-add.py commit : correction 7

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-res = __import__('add_0')
+if __name__ == "__main__":
+    res = __import__('add_0')
 
-a = 1
-b = 2
-print('{:d} + {:d} = {:d}'.format(a, b, res.add(a, b)))
+    a = 1
+    b = 2
+    print('{:d} + {:d} = {:d}'.format(a, b, res.add(a, b)))
