63892fbc186efd5bcce081418e870b1145fcfda7
Joshua Akuna <akunajoshua@gmail.com>
2023-06-09 15:20:30 +0100

    1-calculation.py : correction 4

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
+from calculator_1 import add, sub, mul, div
 if __name__ == "__main__":
-    res = __import__('calculator_1')
 
     a = 10
     b = 5
 
-    print("{:d} + {:d} = {:d}".format(a, b, res.add(a, b)))
-    print("{:d} - {:d} = {:d}".format(a, b, res.sub(a, b)))
-    print("{:d} * {:d} = {:d}".format(a, b, res.mul(a, b)))
-    print("{:d} / {:d} = {:d}".format(a, b, int(res.div(a, b))))
+    print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
+    print("{:d} - {:d} = {:d}".format(a, b, sub(a, b)))
+    print("{:d} * {:d} = {:d}".format(a, b, mul(a, b)))
+    print("{:d} / {:d} = {:d}".format(a, b, int(div(a, b))))
