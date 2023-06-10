cafd28155295df9b75b490634f3294ef4b386292
Joshua Akuna <akunajoshua@gmail.com>
2023-06-10 15:54:19 +0100

    9-max_integer.py : correction 3

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -4,7 +4,7 @@ def max_integer(my_list=[]):
     if len(my_list) == 0:
         return (None)
 
-    max_num = 0
+    max_num = -100000
     for num in my_list:
         if num > max_num:
             max_num = num
