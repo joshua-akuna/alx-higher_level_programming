7b7ff255b07b539bbfd44d2505ea0fe1f6803a1d
Joshua Akuna <akunajoshua@gmail.com>
2023-06-10 17:42:37 +0100

    11-delete_at.py file commit

---
+++ 0x03-python-data_structures/11-delete_at.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+def delete_at(my_list=[], idx=0):
+    if idx < 0 or idx >= len(my_list):
+        return (my_list)
+
+    for index, item in enumerate(my_list):
+        if index == idx:
+            my_list.remove(item)
+    return (my_list)
