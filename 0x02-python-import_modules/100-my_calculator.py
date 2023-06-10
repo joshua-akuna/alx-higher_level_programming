3885bc688f371c5726445c9108da61437ba94e4e
Joshua Akuna <akunajoshua@gmail.com>
2023-06-10 10:06:32 +0100

    100-my_calculator.py : correction 1

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -5,7 +5,7 @@ if __name__ == "__main__":
     argc = len(sys.argv)
     if argc - 1 != 3:
         print("Usage: {} <a> <operator> <b>".format(sys.argv[0]))
-        exit(1)
+        sys.exit(1)
 
     operator = sys.argv[2]
     a = int(sys.argv[1])
@@ -21,6 +21,7 @@ if __name__ == "__main__":
         res = a / b
     else:
         print('Unknown operator. Available operators: +, -, *, /')
-        exit(1)
+        sys.exit(1)
 
     print("{:d} {:s} {:d} = {}".format(a, operator, b, res))
+    sys.exit(0)
