4a75c81fb39ca78cb7db435ab5147300a8349c40
Joshua Akuna <akunajoshua@gmail.com>
2023-06-12 02:22:27 +0100

    6-print_matrix_integer.py : correction 1

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,5 +1,13 @@
 #!/usr/bin/python3
 
 def print_matrix_integer(matrix=[[]]):
-    for row in matrix:
-        print(' '.join("{}".format(num) for num in row))
+    if matrix:
+        for row in matrix:
+            for item in row:
+                if item == row[0]:
+                    print("{:d}".format(item), end="");
+                else:
+                    print(" {:d}".format(item), end="");
+            print()
+    else:
+        print()
