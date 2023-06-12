f84d5bb3e2c9f62bfb4cb1eb8122e537eb7cc3ca
Joshua Akuna <akunajoshua@gmail.com>
2023-06-12 01:31:11 +0100

    13-is_palindrome.c file : correction 3

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -8,58 +8,33 @@
  */
 int is_palindrome(listint_t **head)
 {
-	listint_t *forward_node = *head;
-	int node_count = 0;
-	int forward_node_index, backward_node_index;
-	int fval, bval;
+	listint_t *front, *rear, *next, *prev;
 
 	if (head == NULL || *head == NULL)
 		return (1);
 
-	while (forward_node->next != NULL)
+	front = *head;
+	rear = *head;
+	prev = NULL;
+
+	while (front != NULL && front->next != NULL)
 	{
-		node_count++;
-		forward_node = forward_node->next;
+		front = front->next->next;
+		next = rear->next;
+		rear->next = prev;
+		prev = rear;
+		rear = next;
 	}
 
-	forward_node_index = 0;
-	backward_node_index = node_count;
+	if (front != NULL)
+		rear = rear->next;
 
-	while (forward_node_index < backward_node_index)
+	while (prev != NULL && rear != NULL)
 	{
-		fval = get_value_at_node_index(*head, forward_node_index);
-		bval = get_value_at_node_index(*head, backward_node_index);
-		/* printf("bval: %d ==> fval: %d ?\n", bval, fval); */
-		if (fval != bval)
+		if (prev->n != rear->n)
 			return (0);
-		forward_node_index++;
-		backward_node_index--;
+		prev = prev->next;
+		rear = rear->next;
 	}
-
 	return (1);
-}
-
-/**
- * get_value_at_node_index - retrieves the value of the
- *	n property of a linked list node at a specified index.
- * @head: the head of the linked list.
- * @index: the specified index of the node whose n property
- *	we want to get.
- * Return: the value of the n property of the node at a
- *	specified index.
- */
-int get_value_at_node_index(listint_t *head, int index)
-{
-	int i = 0;
-	listint_t *ptr = head;
-
-	while (ptr->next != NULL)
-	{
-		if (i == index)
-			break;
-		i++;
-		ptr = ptr->next;
-	}
-
-	return (ptr->n);
 }
