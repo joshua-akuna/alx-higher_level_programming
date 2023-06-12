22dfb2ce431fafcecd5c4eb8c171f7862f0e7426
Joshua Akuna <akunajoshua@gmail.com>
2023-06-12 16:00:57 +0100

    100-print_python_list_info.c : correction 1

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -6,20 +6,24 @@
  *	about Python lists.
  * @p: an pointer argument of type PyObject.
  */
-void print_python_list_info(PyObject *p)
-{
+
+
+void print_python_list_info(PyObject *p) {
+	Py_ssize_t size, allocated, i;
 	PyListObject *list = (PyListObject *)p;
-	Py_ssize_t allocated = list->allocated, size = PyList_Size(p), i;
-	PyObject *item;
-	const char *type_name;
+	const char *typeName;
+	PyObject *element;
+
+	size = PyList_Size(p);
+	allocated = list->allocated;
 
 	printf("[*] Size of the Python List = %ld\n", size);
-	printf("[*] Allocated  = %ld\n", allocated);
-
-	for (i = 0; i < size; i++)
-	{
-		item = PyList_GetItem(p, i);
-		*type_name = Py_TYPE(item)->tp_name;
-		printf("Element %ld: %s\n", i. type_name);
+	printf("[*] Allocated = %ld\n", allocated);
+	
+	for (i = 0; i < size; i++) {
+		element = PyList_GetItem(p, i);
+		typeName = Py_TYPE(element)->tp_name;
+		printf("Element %ld: %s\n", i, typeName);
 	}
 }
+
