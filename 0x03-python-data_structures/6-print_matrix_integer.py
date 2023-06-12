92dd36ec04ff3a7de2f157fd590f95d87dbc422e
Joshua Akuna <akunajoshua@gmail.com>
2023-06-12 02:24:40 +0100

    6-print_matrix_integer.py : correction 2 : removed all semicolon

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -5,9 +5,9 @@ def print_matrix_integer(matrix=[[]]):
         for row in matrix:
             for item in row:
                 if item == row[0]:
-                    print("{:d}".format(item), end="");
+                    print("{:d}".format(item), end="")
                 else:
-                    print(" {:d}".format(item), end="");
+                    print(" {:d}".format(item), end="")
             print()
     else:
         print()
