586be0ccc423c408be8edd8b9bf32e996287f182
Joshua Akuna <akunajoshua@gmail.com>
2023-06-14 17:27:09 +0100

    101-square_matrix_map.py : correction 1

--- 0x04-python-more_data_structures/101-square_matrix_map.py
+++ 0x04-python-more_data_structures/101-square_matrix_map.py
@@ -1,5 +1,3 @@
 #!/usr/bin/python3
 def square_matrix_map(matrix=[]):
-    return list(
-            map(lambda row: list(map(lambda item: item ** 2, row)), matrix)
-    )
+    return list(map(lambda row: list(map(lambda i: i ** 2, row)), matrix))
