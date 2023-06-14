8f7bd8f435790fcce1df46ef4c34217f48a497dd
Joshua Akuna <akunajoshua@gmail.com>
2023-06-14 14:57:51 +0100

    7-update_dictionary.py : correction 3

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -1,8 +1,4 @@
-#!/usr/bin/python
-
+#!/usr/bin/python3
 def update_dictionary(a_dictionary, key, value):
-    if a_dictionary[key]:
-        a_dictionary[key] = value
-    else:
-        a_dictionary[key] = value
+    a_dictionary[key] = value
     return a_dictionary
