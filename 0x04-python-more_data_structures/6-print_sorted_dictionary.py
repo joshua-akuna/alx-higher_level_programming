f574575db5a72905b7fa621af97bf8c03ee976ae
Joshua Akuna <akunajoshua@gmail.com>
2023-06-14 14:34:24 +0100

    6-print_sorted_dictionary.py : correction 1

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,4 +1,11 @@
 #!/usr/bin/python3
 def print_sorted_dictionary(a_dictionary):
-    for key in sorted(a_dictionary.keys()):
-        print(a_dictionary[key])
+    sorted_keys = sorted(a_dictionary.keys())
+    for key in sorted_keys:
+        val = a_dictionary[key]
+
+        if isinstance(val, list):
+            val_str = "[" + ", ".join(map(str, val)) + "]"
+        else:
+            val_str = str(val)
+        print("{}: {}".format(key, val_str))
