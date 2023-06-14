5a49f6675d4fad587eb10972f451d432a20aaa99
Joshua Akuna <akunajoshua@gmail.com>
2023-06-14 16:47:44 +0100

    9-multiply_by_2.py : correction 1

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 def multiply_by_2(a_dictionary):
-    new_map = a_dictionary.copy()
-    for key in new_map.keys():
-        new_map[key] = new_map[key] * 2
+    new_map = {}
+    for key in a_dictionary.keys():
+        new_map[key] = a_dictionary[key] * 2
     return (new_map)
