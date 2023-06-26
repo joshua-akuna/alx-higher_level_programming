b958b0e79048dfb89cd45d8267162a6d278f1255
Joshua Akuna <akunajoshua@gmail.com>
2023-06-26 13:12:12 +0100

    102-magic_calculation.py file commit

---
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+def magic_calculation(a, b):
+    res = 0
+    for item in range(1, 3):
+        try:
+            if a <= item:
+                res += a ** b / i
+            else
+            raise Exception('Too far')
+        except Exception:
+            return (a + b)
+    return (res)
