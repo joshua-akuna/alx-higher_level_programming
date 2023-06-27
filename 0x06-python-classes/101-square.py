f2fba0754a439f73cb0d0a41d4c240f68156eaa4
Joshua Akuna <akunajoshua@gmail.com>
2023-06-27 14:00:04 +0100

    101-square.py file commit

---
+++ 0x06-python-classes/101-square.py
@@ -0,0 +1,61 @@
+#!/usr/bin/python3
+"""Definition for the Square class"""
+
+
+class Square:
+    """Defines the constructor for the Square class the initializes
+        the private instance size and position tuple of the instance(self)"""
+    def __init__(self, size=0, position=(0, 0)):
+        self.size = size
+        self.position = position
+
+    """Defines the getter for the private instance self property"""
+    @property
+    def size(self):
+        return (self.__size)
+
+    """Defines the setter for the private instance self property"""
+    @size.setter
+    def size(self, value):
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    """Defines the getter for the private instance position property"""
+    @property
+    def position(self):
+        return self.__position
+
+    """Defines the setter for the private instance position property"""
+    @position.setter
+    def position(self, value):
+        if type(value) is not tuple or len(value) != 2\
+                or type(value[0]) is not int or type(value[1]) is not int\
+                or value[0] < 0 or value[1] < 0:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        self.__position = value
+
+    """The area method returns the area = size * size of the
+        a Square instance"""
+    def area(self):
+        return (self.size * self.size)
+
+    """returns a square of '#' of length size"""
+    def __str__(self):
+        to_string = ''
+        if self.__size == 0:
+            return '\n'
+
+        for item in range(self.position[1]):
+            to_string += '\n'
+        for item in range(self.size):
+            to_string += " " * self.position[0]
+            to_string += "#" * self.size
+            to_string += '\n'
+        return (to_string)
+
+    """prints a square of '#' of length size"""
+    def my_print(self):
+        print(self, end="")
