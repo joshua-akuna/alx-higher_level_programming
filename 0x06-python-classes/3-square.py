5fe02922f605d628e6949bb1da2c4c9354b9a91b
Joshua Akuna <akunajoshua@gmail.com>
2023-06-27 12:31:34 +0100

    3-square.py: correction 1: added documentation

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
+"""The definition of a Square class"""
+
 
 class Square:
+    """The constructor of the Square class
+        that initializes the private instance property size"""
     def __init__(self, size=0):
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
@@ -8,5 +12,7 @@ class Square:
             raise ValueError("size must be >= 0")
         self.__size = size
 
+    """The area method returns the area = size * size of the 
+        a Square instance"""
     def area(self):
         return (self.__size ** 2)
