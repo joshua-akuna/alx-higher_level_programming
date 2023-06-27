04e321499825e9de77c73a4d6627334efaa44e27
Joshua Akuna <akunajoshua@gmail.com>
2023-06-27 12:23:47 +0100

    1-square.py: correction 1: adding documentation

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
+"""The definition of a Square class"""
+
 
 class Square:
+    """The constructor for the Square class"""
     def __init__(self, size):
         self.__size = size
