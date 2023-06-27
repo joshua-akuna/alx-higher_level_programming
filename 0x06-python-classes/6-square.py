9f7d04bffcd98b9c58bab113cace04596e030f63
Joshua Akuna <akunajoshua@gmail.com>
2023-06-27 13:11:31 +0100

    6-square.py: correction 2: updated setter for position

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -35,7 +35,9 @@ class Square:
     """Defines the setter for the private instance position property"""
     @position.setter
     def position(self, value):
-        if type(value) is not tuple or len(value) != 2:
+        if type(value) is not tuple or len(value) != 2\
+                or type(value[0]) is not int or type(value[1]) is not int\
+                or value[0] < 0 or value[1] < 0:
             raise TypeError("position must be a tuple of 2 positive integers")
         self.__postion = value
 
