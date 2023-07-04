0f768cb2487c1672ff26f1577c9b940224bf99a3
Joshua Akuna <akunajoshua@gmail.com>
2023-07-04 13:07:23 +0100

    100-magic_string.py : correction 1
    documantation removed

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,8 +1,4 @@
 #!/usr/bin/python3
-"""This module defines the magic_string function"""
-
-
 def magic_string(text=[]):
-    """The magic string function returns concatenated string"""
     text += ["BestSchool"]
     return ", ".join(text)
