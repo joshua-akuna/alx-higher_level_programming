40ae850d8e3256125766d8628e0458d3e9bbaf53
Joshua Akuna <akunajoshua@gmail.com>
2023-07-04 13:05:45 +0100

    100-magic_string.py file commit

---
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""This module defines the magic_string function"""
+
+
+def magic_string(text=[]):
+    """The magic string function returns concatenated string"""
+    text += ["BestSchool"]
+    return ", ".join(text)
