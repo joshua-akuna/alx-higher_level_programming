a0e4063b3f9a172414b996d3374b589bde957dc8
Joshua Akuna <akunajoshua@gmail.com>
2023-07-04 18:12:56 +0100

    101-locked_class.py file commit

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -8,8 +8,10 @@ class LockedClass:
         new instance attributes, except if the new class
         is called first_name
     """
+
     def __setattr__(self, name, value):
-        err = "'LockedClass' object has no attribue '{}'"
+        """setattr magic method"""
+        n = name
         if name != 'first_name':
-            raise AttributeError(err.format(name))
+            raise AttributeError(f"'LockedClass' object has no attribute '{n}'")
         self.__dict__[name] = value
