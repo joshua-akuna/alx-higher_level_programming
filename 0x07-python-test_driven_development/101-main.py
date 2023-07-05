bcca1e82dd1342ea2d380a79b64e505cfd24d6ee
Joshua Akuna <akunajoshua@gmail.com>
2023-07-05 05:09:42 +0100

    101-lazy_matrix_mul.py : pycodestyle error

--- 0x07-python-test_driven_development/101-main.py
+++ 0x07-python-test_driven_development/101-main.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-lazy_matrix_mul = __import__('101-lazy_matrix_mul').lazy_matrix_mul
 import numpy as np
+lazy_matrix_mul = __import__('101-lazy_matrix_mul').lazy_matrix_mul
 if __name__ == '__main__':
     print(lazy_matrix_mul([[1, 2], [3, 4]], [[1, 2], [3, 4]]))
     print(lazy_matrix_mul([[1, 2]], [[3, 4], [5, 6]]))
