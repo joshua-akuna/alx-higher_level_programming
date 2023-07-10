b9ad2e4a9b95bf93ea793ca4f2d51e2c0a572027
Akuna Joshua <akunajoshua@gmail.com>
2023-07-10 12:58:28 -0700

    9-rectangle.py file commit

---
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""The module defines a Rectangle class"""
+
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """The BaseGeometry class defines a single area function that
+    raises with the message 'area() is not implemented'
+    """
+
+    def __init__(self, width, height):
+        """A constructor for the BaseGeometry class"""
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
+
+
+    def area(self):
+        """Calculates the area of the Rectangle object"""
+        return self.__width * self.__height
+
+
+    def print():
+        """The print function prints the dimensions of the Rectangle instance"""
+        print(self)
+
+
+    def __str__(self):
+        """Implements the toString() method"""
+        return f"[Rectangle] {self.__width}/{self.__height}"
