c698265698118d5af6aa29ff956c2ac5b9c73ee4
Akuna Joshua <akunajoshua@gmail.com>
2023-07-11 08:33:25 -0700

    3-to_json_string.py file commit

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python
+"""The module contains the to_json_string function"""
+
+import json
+
+
+def to_json_string(my_obj):
+    """The function the JSON representation of an object"""
+    return json.dumps(my_obj)
