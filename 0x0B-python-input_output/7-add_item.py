46fce198682e63c8b44c4d6ccc4c4a30336c3ee5
Joshua Akuna <akunajoshua@gmail.com>
2023-07-12 00:24:57 +0100

    7-add_item.py file commit

---
+++ 0x0B-python-input_output/7-add_item.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+""""""
+
+# imports the required modules
+import json
+from sys import argv
+save_to_json = __import__('5-save_to_json_file').save_to_json_file
+load_from_json = __import__('6-load_from_json_file').load_from_json_file
+
+# the file to save and load the list from
+filename = 'add_item.json'
+
+try:
+    _list = load_from_json(filename)
+except (ValueError, FileNotFoundError):
+    _list = []
+
+_list.extend(argv[1:])
+save_to_json(_list, filename)
