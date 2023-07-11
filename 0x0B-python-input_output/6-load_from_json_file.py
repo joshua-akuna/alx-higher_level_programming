8223aaa4d450a7baf72481d90568fcb36a85958b
Joshua Akuna <akunajoshua@gmail.com>
2023-07-11 20:53:37 +0100

    6-load_from_json_file.py commit

---
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""The module defines the load_from_json_file function"""
+import json
+
+
+def load_from_json_file(filename):
+    """The function creates an Object from a 'JSON file'"""
+    with open(filename, 'r', encoding='UTF8') as f:
+        json.loads(f.read())
