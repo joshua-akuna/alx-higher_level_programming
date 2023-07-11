62c199cec1945668d4c7d89b8c3723ab0a06a83f
Akuna Joshua <akunajoshua@gmail.com>
2023-07-11 08:56:33 -0700

    4-from_json_string.py file commit

--- 0x0B-python-input_output/4-from_json_string.py
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
-""""""
+"""The module contains the from_json_string function"""
 import json
 
 
 def from_json_string(my_str):
-    """"""
+    """The function returns an object (Python data structure)
+    representation of a json string
+    """
     return json.loads(my_str)
