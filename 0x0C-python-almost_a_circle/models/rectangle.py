9fb4ca830f0ad71a17da17512f7560ca6e1d0ac8
Joshua Akuna <akunajoshua@gmail.com>
2023-07-17 12:31:01 +0100

    initial project commit

---
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+
+from models.base import Base
+
+class Rectangle(Base):
+    # 
+    def __init__(self, width, height, x=0, y=0, id=None):
+        self.__width = width
+        self.__height = height
+        self.__x = x
+        self.__y = y
+        super().__init__(id)
+
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        self.__width = value
+
+    @property
+    def height(self):
+        return self.__width
+
+    @height.setter
+    def height(self, value):
+        self.__width = value
