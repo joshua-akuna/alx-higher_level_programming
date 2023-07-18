7b25508e5c57b211d845fb9fd72c69e83399e375
Joshua Akuna <akunajoshua@gmail.com>
2023-07-18 07:57:19 +0100

    15-dictionary to json string commit

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 
+import json
+
 class Base:
     __nb_objects = 0
 
@@ -9,3 +11,9 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        if list_dictionaries is None:
+            return "[]"
+        return json.dumps(list_dictionaries)
