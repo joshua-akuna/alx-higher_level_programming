8a3f9c205f17cff37e60c2c638249d3a0cee2676
Joshua Akuna <akunajoshua@gmail.com>
2023-07-18 06:42:36 +0100

    12-square update commit

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -6,6 +6,16 @@ class Square(Rectangle):
     def __init__(self, size, x=0, y=0, id=None):
         super().__init__(size, size, x, y, id)
     
+    def update(self, *args, **kwargs):
+        if args:
+            arr = ['id', 'size', 'x', 'y']
+            for i in range(len(args)):
+                setattr(self, arr[i], args[i])
+        else:
+            for key, value in kwargs.items():
+                setattr(self, key, value)
+
+
     @property
     def size(self):
         return self.width
