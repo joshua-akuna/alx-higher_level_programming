a0c2349565bf91a7fcef8523b9197faa4216406e
Joshua Akuna <akunajoshua@gmail.com>
2023-07-19 19:09:00 +0100

    square module documentation commit

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -1,12 +1,22 @@
 #!/usr/bin/python
-
+"""The square model defines the Square class"""
 from models.rectangle import Rectangle
 
+
 class Square(Rectangle):
+    """
+    The definition for the Square sub class with
+    properties size, x, y and id.
+    """
     def __init__(self, size, x=0, y=0, id=None):
+        """constructor for the initialization of Square instances"""
         super().__init__(size, size, x, y, id)
     
     def update(self, *args, **kwargs):
+        """
+        This funtion is used to update the properties of
+        Square instances
+        """
         if args:
             arr = ['id', 'size', 'x', 'y']
             for i in range(len(args)):
@@ -17,14 +27,18 @@ class Square(Rectangle):
 
     @property
     def size(self):
+        """getter for the size property"""
         return self.width
 
     @size.setter
     def size(self, value):
+        """setter for the size property"""
         self.width = value
         self.height = value
 
     def to_dictionary(self):
+        """returns the dictionary representation of the current
+        Square instance."""
         return {
             'id': self.id,
             'size': self.size,
