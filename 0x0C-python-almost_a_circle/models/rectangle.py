70cced9c1e1be16d73271acc0d8b70a9c9edc980
Joshua Akuna <akunajoshua@gmail.com>
2023-07-19 19:00:45 +0100

    rectangle module documentation commit

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -1,10 +1,18 @@
 #!/usr/bin/python3
-
+"""
+This module defines the Rectangle sub class which
+inherits the Base super class
+"""
 from models.base import Base
 
+
 class Rectangle(Base):
-    # 
+    """
+    The Rectangle class with public instance properties
+    width, height, x, y and id.
+    """
     def __init__(self, width, height, x=0, y=0, id=None):
+        """This constructor initializes a Rectangle instance"""
         self.width = width
         self.height = height
         self.x = x
@@ -12,9 +20,11 @@ class Rectangle(Base):
         super().__init__(id)
 
     def area(self):
+        """The area function returns the area of the rectangle"""
         return self.height * self.width
 
     def display(self):
+        """The display property displays the rectangle using #'s"""
         for i in range(self.y):
             print()
         for i in range(self.height):
@@ -22,6 +32,10 @@ class Rectangle(Base):
             print('#' * self.width)
 
     def update(self, *args, **kwargs):
+        """
+        The update property is used to update the properties of
+        a rectangle instance.
+        """
         size = len(args)
         if size > 0:
             self.id = args[0]
@@ -39,6 +53,10 @@ class Rectangle(Base):
                 setattr(self, key, value)
 
     def to_dictionary(self):
+        """
+        This function returns the dictionary representation
+        of the current rectangle instnace.
+        """
         return {
             'id': self.id,
             'width': self.width,
@@ -48,6 +66,10 @@ class Rectangle(Base):
         }
 
     def __str__(self):
+        """
+        This function returns a string representation of the
+        the current rectangle instance.
+        """
         return "[{}] ({}) {}/{} - {}/{}".format(
             self.__class__.__name__,
             self.id,
@@ -59,10 +81,12 @@ class Rectangle(Base):
 
     @property
     def width(self):
+        """getter for the width property"""
         return self.__width
 
     @width.setter
     def width(self, value):
+        """setter for the width property"""
         if type(value) is not int:
             raise TypeError('width must be an integer')
         if value <= 0:
@@ -71,10 +95,12 @@ class Rectangle(Base):
 
     @property
     def height(self):
+        """getter for the height property"""
         return self.__height
 
     @height.setter
     def height(self, value):
+        """setter for the width property"""
         if type(value) is not int:
             raise TypeError('height must be an integer')
         if value <= 0:
@@ -83,10 +109,12 @@ class Rectangle(Base):
 
     @property
     def x(self):
+        """getter for the x property"""
         return self.__x
 
     @x.setter
     def x(self, value):
+        """setter for the x property"""
         if type(value) is not int:
             raise TypeError('x must be an integer')
         if value < 0:
@@ -95,10 +123,12 @@ class Rectangle(Base):
 
     @property
     def y(self):
+        """getter for the y property"""
         return self.__y
 
     @y.setter
     def y(self, value):
+        """setter for the y property"""
         if type(value) is not int:
             raise TypeError('y must be an integer')
         if value < 0:
