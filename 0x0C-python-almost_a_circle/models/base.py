446699f1724a942387aef94fa22ba926c79d029c
Joshua Akuna <akunajoshua@gmail.com>
2023-07-19 18:42:56 +0100

    base module documentation added

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,11 +1,20 @@
 #!/usr/bin/python3
-
+""" This module defines the Base super class """
 import json
 
+
 class Base:
+    """
+    This is the base (super) class for all other classes
+    in this project.
+    """
     __nb_objects = 0
 
     def __init__(self, id=None):
+        """
+        This is the constructor of the Base class which
+        initializes its public instance id property
+        """
         if id is not None:
             self.id = id
         else:
@@ -14,12 +23,21 @@ class Base:
 
     @staticmethod
     def to_json_string(list_dictionaries):
+        """
+        a static method that converts a list of dictionaries
+        to its JSON string representation and returns it if it
+        exists else returns an empty string.
+        """
         if list_dictionaries is None:
             return "[]"
         return json.dumps(list_dictionaries)
 
     @classmethod
     def save_to_file(cls, list_objs):
+        """
+        serializes the list of base objects to a file in their JSON
+        format if they exist else serializes an empty list.
+        """
         filename = cls.__name__ + ".json"
         arr = []
 
@@ -32,12 +50,17 @@ class Base:
 
     @classmethod
     def create(cls, **dictionary):
+        """
+        create a new base sub class whose properties are updated
+        with the dictionary argument property
+        """
         dummy = cls(1, 1)
         dummy.update(**dictionary)
         return dummy
 
     @classmethod
     def load_from_file(cls):
+        """ deserializes persisted properties back to base sub instances"""
         list_of_base_objs = []
 
         filename = cls.__name__ + '.json'
