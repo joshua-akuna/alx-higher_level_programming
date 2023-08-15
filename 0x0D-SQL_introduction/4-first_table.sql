8d067244973585758287b91413535ffe5284f638
Akuna Joshua <akunajoshua@gmail.com>
2023-08-15 04:44:51 -0700

    4-first_table.sql : correction : added comments

--- 0x0D-SQL_introduction/4-first_table.sql
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -1,1 +1,4 @@
-CREATE TABLE first_table (id INT, name VARCHAR(256))
+-- the scripts creates the table called first_table in
+-- the current database in my MySQL server.
+-- the script will not fail if the table already exists
+CREATE TABLE IF NOT EXISTS first_table (id INT, name VARCHAR(256))
