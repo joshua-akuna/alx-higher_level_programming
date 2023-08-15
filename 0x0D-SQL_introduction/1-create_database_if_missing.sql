a7bd4b2c4d3bf008d3fa420c9fb270dc7a65cf32
Akuna Joshua <akunajoshua@gmail.com>
2023-08-15 04:30:07 -0700

    1-create_database_if_missing.sql : correction : comments added

--- 0x0D-SQL_introduction/1-create_database_if_missing.sql
+++ 0x0D-SQL_introduction/1-create_database_if_missing.sql
@@ -1,1 +1,3 @@
-CREATE DATABASE hbtn_0c_0
+-- the script creates the database hbtn_0c_0 in my MySQL server
+-- if the database already exists,the script will not fail
+CREATE DATABASE IF NOT EXISTS hbtn_0c_0
