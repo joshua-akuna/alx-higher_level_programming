af3c4d7c4404ca0b73b516c63f76f411b2d6e2a2
Akuna Joshua <akunajoshua@gmail.com>
2023-08-15 06:21:00 -0700

    10-top_score.sql file commit

---
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -0,0 +1,5 @@
+-- the script lists all the records of the table second table
+-- in the database named hbtn_0c_0 in my MySQL server
+-- results should display the score and the name 
+-- results should be ordered by score in descending order
+SELECT score, name FROM second_table ORDER BY score DESC
