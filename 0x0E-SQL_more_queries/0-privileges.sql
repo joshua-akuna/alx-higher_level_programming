89998ae994a09880f9f876cf8df70e8986239c30
Akuna Joshua <akunajoshua@gmail.com>
2023-08-16 04:40:37 -0700

    0-privileges.sql : correction : output error

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -2,5 +2,5 @@
 -- user_0d_1 and user_0d_2 on localhost
 -- shows GRANTS for user_0d_1
 SHOW GRANTS FOR user_0d_1@localhost;
--- shows GRANTS for user_0d_1
+-- shows GRANTS for user_0d_2
 SHOW GRANTS FOR user_0d_2@localhost;
