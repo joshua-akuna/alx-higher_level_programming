04e86e5473e30858324e36423bd5fe55d69415a9
Akuna Joshua <akunajoshua@gmail.com>
2023-08-16 04:37:47 -0700

    0-privileges.sql : correction : output error

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,4 +1,6 @@
 -- the script lists all privileges for the MySQL users
 -- user_0d_1 and user_0d_2 on localhost
+-- shows GRANTS for user_0d_1
 SHOW GRANTS FOR user_0d_1@localhost;
+-- shows GRANTS for user_0d_1
 SHOW GRANTS FOR user_0d_2@localhost;
