17ca4207e82ee3965b96c57a28c9520c50cb5d1c
Akuna Joshua <akunajoshua@gmail.com>
2023-08-16 08:43:48 -0700

    101-not_a_comedy.sql file commit

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,8 +1,5 @@
 -- the scripts creates the MySQL server user user_0d_1
 --	password should be set to user_0d_1_pwd
-CREATE USER IF NOT EXISTS user_0d_1@localhost
-IDENTIFIED BY "user_0d_1_pwd";
+CREATE USER IF NOT EXISTS user_0d_1@localhost IDENTIFIED BY "user_0d_1_pwd";
 --	user_0d_1 should have all privileges on the server
-GRANT ALL PRIVILEGES
-ON *.*
-TO user_0d_1@localhost;
+GRANT ALL PRIVILEGES ON * . * TO user_0d_1@localhost;
