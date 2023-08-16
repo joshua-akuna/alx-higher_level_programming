a5a52b1ff15f80748282654700b09ac5521c45eb
Akuna Joshua <akunajoshua@gmail.com>
2023-08-16 04:54:53 -0700

    1-create_user.sql : correction : output error

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -3,6 +3,6 @@
 CREATE USER IF NOT EXISTS user_0d_1@localhost
 IDENTIFIED BY "user_0d_1_pwd";
 --	user_0d_1 should have all privileges on the server
-GRANT ALL
+GRANT ALL PRIVILEGES
 ON *.*
 TO user_0d_1@localhost;
