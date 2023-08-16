acc2fa8a1a9ea7fe826077392dfd31f24a609148
Akuna Joshua <akunajoshua@gmail.com>
2023-08-16 04:33:33 -0700

    1-create_user.sql : correction : output error

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,8 +1,8 @@
 -- the scripts creates the MySQL server user user_0d_1
---	user_0d_1 should have all privileges on the server
 --	password should be set to user_0d_1_pwd
-CREATE USER IF NOT EXIST user_0d_1@localhost
-IDENTIFIED BY user_0d_1_pwd;
+CREATE USER IF NOT EXISTS user_0d_1@localhost
+IDENTIFIED BY "user_0d_1_pwd";
+--	user_0d_1 should have all privileges on the server
 GRANT ALL
 PRIVILEGES ON *.*
-FOR user_0d_1@localhost;
+TO user_0d_1@localhost;
