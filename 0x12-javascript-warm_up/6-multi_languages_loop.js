8f582dbd58dc8e75bec45a74d9d5dcf806743649
Akuna Joshua <akunajoshua@gmail.com>
2023-09-11 13:33:38 +0100

    6-multi_languages_loop.js file commit

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+/**
+ * The script prints 3 lines using an array and a loop.
+ */
+const lines = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+
+for (const line of lines) { console.log(line); }
