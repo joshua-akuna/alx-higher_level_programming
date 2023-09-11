879641194173210eb0ab3e8c591d1c0ef5f671a5
Akuna Joshua <akunajoshua@gmail.com>
2023-09-11 12:26:27 +0100

    1-multi_languages.js : correction : semistandard requirement error

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 /**
  * The script prints 3 lines
- * 	-the first line: "C is fun"
- * 	-the second line: "Python is cool"
- * 	-the third line: "JavaScript is amazing"
+ *  -the first line: "C is fun"
+ *  -the second line: "Python is cool"
+ *  -the third line: "JavaScript is amazing"
  */
 
 console.log('C is fun');
