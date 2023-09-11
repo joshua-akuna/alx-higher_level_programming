957b8644f731aa2bd8cf2f547c5c47b85c2575d6
Akuna Joshua <akunajoshua@gmail.com>
2023-09-11 12:18:36 +0100

    1-multi_languages.js file commit

---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+/**
+ * The script prints 3 lines
+ * 	-the first line: "C is fun"
+ * 	-the second line: "Python is cool"
+ * 	-the third line: "JavaScript is amazing"
+ */
+
+console.log('C is fun');
+console.log('Python is cool');
+console.log('JavaScript is amazing');
