4afc0d0ec9fb3430fdbfe943d078ce3307894df2
Akuna Joshua <akunajoshua@gmail.com>
2023-09-11 14:27:14 +0100

    9-add.js : correction : output error

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -6,7 +6,7 @@ const argv = require('process').argv;
 const num1 = parseInt(argv[2]);
 const num2 = parseInt(argv[3]);
 
-if (!isNaN(num1) && !isNaN(num2)) { console.log(add(num1, num2)); }
+console.log(add(num1, num2));
 
 function add (a, b) {
   return a + b;
