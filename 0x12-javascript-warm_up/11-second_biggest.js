a97256e6ff1ca4592bcdb468a91c9b8da1e6f75a
Akuna Joshua <akunajoshua@gmail.com>
2023-09-11 17:35:31 +0100

    11-second_biggest.js : correction : output error

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -2,12 +2,10 @@
 /**
  * The script searches for the second largest integer in a list of arguments
  */
-const argv = require('process').argv;
-
-const newArray = [];
-for (let i = 2; i < argv.length; i++) {
-  newArray.push(parseInt(argv[i]));
+if (process.argv.length <= 3) {
+  console.log('0');
+} else {
+  const newArray = process.argv.slice(2).map(Number);
+  newArray.sort().reverse();
+  console.log(newArray[1]);
 }
-
-newArray.sort().reverse();
-console.log(newArray[1] ? newArray[1] : 0);
