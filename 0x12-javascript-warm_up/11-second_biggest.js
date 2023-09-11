e17bd4fcdde156cb402fbe0c2945de49685ee447
Akuna Joshua <akunajoshua@gmail.com>
2023-09-11 17:52:46 +0100

    11-second_biggest.js file commit

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -2,12 +2,9 @@
 /**
  * The script searches for the second largest integer in a list of arguments
  */
-if (process.argv.length <= 3) {
-  console.log(0);
-} else {
-  const newArray = process.argv
-	.map(Number)
-	.slice(2, process.argv.length)
-	.sort((a, b) => a - b)
-  console.log(newArray[newArray.length - 2]);
-}
+const list = process.argv
+  .map(Number)
+  .slice(2, process.argv.length)
+  .sort((a, b) => a - b)
+  .reverse();
+console.log(list[1] ? list[1] : 0);
