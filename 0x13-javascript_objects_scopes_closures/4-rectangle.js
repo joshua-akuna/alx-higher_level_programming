956c1fd761425ec96c117042977370b3ca48614f
Akuna Joshua <akunajoshua@gmail.com>
2023-09-13 00:15:58 +0100

    -rectangle.js : correction : semistandard

--- 0x13-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -3,34 +3,33 @@
  * The script defines a Rectangle class
  */
 class Rectangle {
-	constructor(w, h) {
-		if (w > 0 && h > 0)
-		{
-			this.width = w;
-			this.height = h;
-		}
-	}
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
 
-	print() {
-		for (let i = 0; i < this.height; i++) {
-			let line = '';
-			for (let j = 0; j < this.width; j++) {
-				line += 'X';
-			}
-			console.log(line);
-		}
-	}
+  print () {
+    for (let i = 0; i < this.height; i++) {
+      let line = '';
+      for (let j = 0; j < this.width; j++) {
+        line += 'X';
+      }
+      console.log(line);
+    }
+  }
 
-	rotate() {
-		let temp = this.width;
-		this.width = this.height;
-		this.height = temp;
-	}
+  rotate () {
+    const temp = this.width;
+    this.width = this.height;
+    this.height = temp;
+  }
 
-	double() {
-		this.width *= 2;
-		this.height *= 2;
-	}
+  double () {
+    this.width *= 2;
+    this.height *= 2;
+  }
 }
 
 module.exports = Rectangle;
