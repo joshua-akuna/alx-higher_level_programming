1550dfc4f4a4c169f7190bc37af58fa92c5e70b0
Akuna Joshua <akunajoshua@gmail.com>
2023-09-12 12:08:09 +0100

    1-rectangle.js files commit

---
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+/**
+ * The script defines a Rectangle class
+ */
+class Rectangle {
+	constructor (w, h) {
+		this.width = w;
+		this.height = h;
+	}
+}
+
+module.exports = Rectangle;
