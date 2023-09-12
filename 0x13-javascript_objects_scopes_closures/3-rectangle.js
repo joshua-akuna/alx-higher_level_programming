1ed2cefa66e9f6e81229b1df1af9a05e7654f3bf
Akuna Joshua <akunajoshua@gmail.com>
2023-09-12 12:09:20 +0100

    3-rectangle.js files commit

---
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,25 @@
+#!/usr/bin/node
+/**
+ * The script defines a Rectangle class
+ */
+class Rectangle {
+	constructor(w, h) {
+		if (w > 0 && h > 0)
+		{
+			this.width = w;
+			this.height = h;
+		}
+	}
+
+	print() {
+		for (let i = 0; i < this.height; i++) {
+			let line = '';
+			for (let j = 0; j < this.width; j++) {
+				line += 'X';
+			}
+			console.log(line);
+		}
+	}
+}
+
+module.exports = Rectangle;
