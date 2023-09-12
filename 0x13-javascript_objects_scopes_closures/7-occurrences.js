bf216ec69ab5c39a084d077062d103bd121c1b2f
Akuna Joshua <akunajoshua@gmail.com>
2023-09-12 12:38:45 +0100

    7-occurrences.js : correction : output error

--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -1,6 +1,9 @@
 #!/usr/bin/node
 
 exports.nbOccurrences = function (list, searchElement) {
-	const occurrences = list.filter((item)=> item === searchElement);
-	return occurrences.length;
+	let mode = 0;
+	for (let item of list) {
+		if (item === searchElement) {mode++}
+	}
+	return mode;
 }
