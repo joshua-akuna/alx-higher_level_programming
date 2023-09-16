540b6a7280e0f5d9be39f0659f8106e427be9bf7
Akuna Joshua <akunajoshua@gmail.com>
2023-09-16 16:47:36 +0100

    2-my_filter_states.py file commit

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -17,7 +17,8 @@ if __name__ == "__main__":
                 charset='utf8'
         )
         cur = conn.cursor()
-        cur.execute("SELECT * FROM states WHERE name = %s", (sys.argv[4],))
+        stmt = "SELECT * FROM states WHERE name='{}'".format(sys.argv[4])
+        cur.execute(stmt)
         rs = cur.fetchall()
 
         for row in rs:
