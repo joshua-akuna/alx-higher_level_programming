dcf8fff0b7bbe41810d419e514093c04c783c50b
Akuna Joshua <akunajoshua@gmail.com>
2023-09-17 04:11:27 +0100

    model_city.py file commit

--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -1,10 +1,15 @@
 #!/usr/bin/python3
-
+"""
+The module defines the City class
+"""
 from model_state import Base
 from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.orm import relationship
 
 class City(Base):
+    """
+    The City class, a Base subclass
+    """
     __tablename__ = 'cities'
 
     id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
