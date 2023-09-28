13884d6bfe3f12c113f208f27c179f0bd9b2979a
Akuna Joshua <akunajoshua@gmail.com>
2023-09-28 22:31:39 +0100

    4-header.sh : correction : shellcheck

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # sends a GET request to a server with the header variable X-School-User-Id with value 98 and display the body of the response
-curl -s -H "X-School-User-Id: 98" $1
+curl -s -H "X-School-User-Id: 98" "$1"
