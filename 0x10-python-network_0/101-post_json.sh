c854204a50c275ec44508d6cc5420bc948b3bd2a
Akuna Joshua <akunajoshua@gmail.com>
2023-09-28 11:06:22 -0700

    101-post_json.sh : correction : requirement error

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 # sends a json POST requedt to a URL and display the response body
-json=$(cat "$2")
-curl -s -X "POST" -H "Content-Type: application/json" -d "$json" "$1"
+curl -s -X "POST" -H "Content-Type: application/json" -d "$(cat "$2")" "$1"
