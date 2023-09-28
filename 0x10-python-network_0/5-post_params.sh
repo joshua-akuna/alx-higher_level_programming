bdd40e29b262bfa2486b382125e18798d4d4cd3d
Akuna Joshua <akunajoshua@gmail.com>
2023-09-28 10:34:58 -0700

    5-post_params.sh : correction : requirements error

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -3,3 +3,4 @@
 # The variable email must be sent with the value test@gmail.com
 # The variable subject must be sent with the value "I will always be here for PLD"
 curl -s -X "POST" -d "email=test@gmail.com&subject=I will always be here for PLD" "$1"
+
