06acee852bde08b71243b8b8bd85fada4eb4ad78
Akuna Joshua <akunajoshua@gmail.com>
2023-09-29 12:30:36 +0100

    0-hbtn_status.py : correction : ouput error

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -6,6 +6,6 @@ import urllib.request
 with urllib.request.urlopen("https://alx-intranet.hbtn.io/status") as response:
     body = response.read()
     print("Body response:")
-    print("    - type: {}".format(type(body)))
-    print("    - body: {}".format(body))
-    print("    - utf8 content: {}".format(body.decode('utf-8')))
+    print("\t- type: {}".format(type(body)))
+    print("\t- body: {}".format(body))
+    print("\t- utf8 content: {}".format(body.decode('utf-8')))
