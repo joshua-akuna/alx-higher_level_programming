1fd1b56016d318a9b6249fb4538adffc1e4bc5e1
Akuna Joshua <akunajoshua@gmail.com>
2023-09-29 08:02:22 +0100

    102-catch_me.sh : correction : output error

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # The script sends a request to 0.0.0.0:5000/catch_me and causes the response "You got me!"
-curl -o /dev/null -sw "You got me!" 0.0.0.0:5000/catch_me
+curl -o /dev/null -sw "You find me!" 0.0.0.0:5000/catch_me
