71ac7cbf9ae44cde6ea40baa15899c94a35edbe7
Akuna Joshua <akunajoshua@gmail.com>
2023-09-29 19:05:50 +0100

    10-my_github.py : correction : module doc

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
+"""The scripts takes github credentials as arguments and uses the
+    GITHUB API to display the ID of the authenticated user
+"""
 
 import sys
 import requests
 
 
 if __name__ == '__main__':
-    if len(sys.argv) == 3:
-        auth = {"username": sys.argv[1], "password": sys.argv[2]}
-        res = requests.get(f"https://api.github.com/users/{sys.argv[1]}", auth)
-        print(res.json().get('id'))
+    auth = {"username": sys.argv[1], "password": sys.argv[2]}
+    res = requests.get(f"https://api.github.com/users/{sys.argv[1]}", auth)
+    print(res.json().get('id'))
