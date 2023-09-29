1240c2d830c83e156fedd31ebd9c90666fb51ee5
Akuna Joshua <akunajoshua@gmail.com>
2023-09-29 07:51:21 -0700

    6-post_email.py : correction : pycodestyle

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -7,5 +7,5 @@ import sys
 
 
 if __name__ == '__main__':
-    res = requests.post(sys.argv[1], {"email" : sys.argv[2]})
+    res = requests.post(sys.argv[1], {"email": sys.argv[2]})
     print(res.text)
