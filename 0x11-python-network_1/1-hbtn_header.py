1328782ede109bc6bb7b694f89168663f4d035fa
Akuna Joshua <akunajoshua@gmail.com>
2023-09-29 13:03:53 +0100

    1-hbtn_header.py : correction : requirement error

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
-"""
-    sends a request to a URL argument and displays the value of the
-    X-Request-Id variable in the response header
-"""
+"""sends a request to a URL argument and displays the value of the
+    X-Request-Id variable in the response header"""
 import urllib.request
 import sys
 
