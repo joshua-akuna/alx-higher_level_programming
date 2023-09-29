443302227e2e5fb225a9c95093f10fe82074e814
Akuna Joshua <akunajoshua@gmail.com>
2023-09-29 12:45:32 +0100

    1-hbtn_header.py file commit

---
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -0,0 +1,10 @@
+#!/uar/bin/python3
+"""
+    sends a request to a URL argument and displays the value of the
+    X-Request-Id variable in the response header
+"""
+import urllib.request
+import sys
+
+with urllib.request.urlopen(sys.argv[1]) as res:
+    print(res.headers["X-Request-Id"])
