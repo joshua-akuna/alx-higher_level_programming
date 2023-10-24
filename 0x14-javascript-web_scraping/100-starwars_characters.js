aacfef32aa07b419e2ecdc71487bb2c1fefff1b9
Akuna Joshua <akunajoshua@gmail.com>
2023-10-24 16:23:32 +0100

    100-starwars_characters.js file added

---
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -0,0 +1,34 @@
+#!/usr/bin/node
+/**
+ * The script prints all characters of a Star Wars movie:
+ *    - The first argument is the Movie ID - example: 3 = "Return of the Jedi"
+ *    - Display one character name by line
+ *    - You must use the StartWars API
+ *    - You must use the module request
+ */
+
+const request = require('request');
+
+if (process.argv.length === 3) {
+  const URL = `https://swapi-api.alx-tools.com/api/films/${process.argv[2]}`;
+  request(URL, (err, response, body) => {
+    if (err) {
+      console.log(err);
+      return;
+    }
+
+    const data = JSON.parse(body);
+    const characters = data.characters;
+
+    for (const character of characters) {
+      request(character, (err, response, body) => {
+        if (err) {
+          console.log(err);
+          return;
+        }
+        const actor = JSON.parse(body);
+        console.log(actor.name);
+      });
+    }
+  });
+}
