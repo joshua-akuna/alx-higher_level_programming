6363a884b92bd3818ac265f76d73b6ccb919ca76
Akuna Joshua <akunajoshua@gmail.com>
2023-11-01 16:24:57 +0100

    4-script.js file added

---
+++ 0x15-javascript-web_jquery/4-script.js
@@ -0,0 +1,11 @@
+$('document').ready(() => {
+  $('DIV#toggle_header').click(() => {
+    if ($('header').hasClass('red')) {
+      $('header').removeClass('red');
+      $('header').addClass('green');
+    } else {
+      $('header').removeClass('green');
+      $('header').addClass('red');
+    }
+  });
+});
