34ee9a798bd27dabd0fbc045651bd8a108d1b281
Akuna Joshua <akunajoshua@gmail.com>
2023-11-01 16:22:23 +0100

    0-script.js : correction : semistandard

--- 0x15-javascript-web_jquery/0-script.js
+++ 0x15-javascript-web_jquery/0-script.js
@@ -1,2 +1,4 @@
-const header = document.querySelector('header');
-header.style.color = '#FF0000';
+document.addEventListener('DOMContentLoaded', function () {
+  const header = document.querySelector('header');
+  header.style.color = '#FF0000';
+});
